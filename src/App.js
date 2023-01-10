24347192ba2c0a8723c433161d50f1ca9a710e94
SausageOG <sauageog@gmail.com>
2023-01-10 20:39:21 +0200

    final

--- src/App.js
+++ src/App.js
@@ -26,25 +26,22 @@ function App() {
     return (
         <div className='App'>
             <div className="nav_bar">
-              <label className="nav_label">ABOUT ME</label>
-              <label className="nav_label">CONTACT</label>
-              <label className="nav_label">WEB BY</label>
+              <a className="nav_label" href="#about_me">ABOUT ME</a>
+              <a className="nav_label" href="#footer">CONTACT</a>
+              <a className="nav_label" href="https://www.instagram.com/maks.dushka/">WEB BY MKSDU</a>
             </div>
             <div className='frame_1'>
-                <img src={logo} alt='' />
+              <img src={logo} alt='' />
             </div>
             <label className='pfe_label'>PICTURES FOR EVENTS</label>
             <div className='frame_2'>
                 <div className='row1'>
                     <img src={afro} alt=''></img>
-
                     <img src={club} alt=''></img>
-
                     <img src={hiphop} alt=''></img>
                 </div>
                 <div className='row2'>
                     <img src={oldSchool} alt=''></img>
-
                     <img src={hiphopNight} alt=''></img>
                 </div>
             </div>
@@ -65,18 +62,17 @@ function App() {
               </div>
             </div>
             <hr id='split_line'/>
-            <div className="about_me">
+            <div id="about_me">
               <h1>ARTYOM “SDRIX” DENISYUK GRAPHIC DESIGNER</h1>
               <h3>I am 18 years old from the city of Riga/Latvia. I have been working with Photoshop for over 8 years. I have experience of working with different artists, clubs and also developed my own design company.</h3>
             </div>
             <div id="footer">
               <div className="media_cont">
-                <img src={ig}></img>
-                <img src={telegram}></img>
-                <img src={gmail}></img>
+                <a className="media_link" href="https://www.instagram.com/shelovessdrix/"><img alt="" src={ig}></img></a>
+                <a className="media_link" href="tg:@sdrix163"><img alt="" src={telegram}></img></a>
+                <a className="media_link" href="mailto:lapefx@gmail.com"><img alt="" src={gmail}></img></a>
               </div>
-              
-              <label id="footer_sign">w-design by mksdu.</label>
+              <a href="https://www.instagram.com/maks.dushka/" id="footer_sign">w-design by mksdu.</a>
             </div>
         </div>
     );
